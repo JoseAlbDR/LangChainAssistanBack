5a4c94ec01568b886c540f74642a25ec92b8a929
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-27 20:36:28 +0100

    feat: add document info to envs adapter

--- src/config/envs.adapter.ts
+++ src/config/envs.adapter.ts
@@ -2,12 +2,24 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
+  //* Database
   DATABASE_URL: get('PORT')
     .default('postgresql://user:secret@localhost:5432/vector-db')
     .asString(),
-  OPENAI_API_KEY: get('OPENAI_API_KEY').required().asString(),
   DB_PASSWORD: get('DB_PASSWORD').default('secret').asString(),
   DB_USER: get('DB_USER').default('user').asString(),
   DB_NAME: get('DB_NAME').default('vector-db').asString(),
+
+  //* OPENAI API KEY
+  OPENAI_API_KEY: get('OPENAI_API_KEY').required().asString(),
+
+  //* DOCUMENT INFO
+  DOCUMENT_PATH: get('DOCUMENT_PATH').default('./data/').asString(),
+  DOCUMENT_NAME: get('DOCUMENT_NAME').default('document').asString(),
+  DOCUMENT_EXTENSION: get('DOCUMENT_EXTENSION')
+    .default('.txt')
+    .asEnum(['.txt', '.pdf']),
+
+  //* APP PORT
   PORT: get('PORT').default(3000).asPortNumber(),
 };
