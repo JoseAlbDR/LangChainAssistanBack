1b4b138a110dad1705db7162c73b92bb8cc9e6df
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-03-13 19:18:13 +0100

    feat: handle class validator bad request errors

--- src/errors/exception-filter.ts
+++ src/errors/exception-filter.ts
@@ -4,12 +4,35 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
+    console.log({ exception });
+
+    if (exception instanceof BadRequestException) {
+      const validationErrors = exception.getResponse()['message'];
+      if (Array.isArray(validationErrors)) {
+        // Si hay errores de validación, devolverlos como respuesta
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+
+        response.status(HttpStatus.BAD_REQUEST).json({
+          statusCode: HttpStatus.BAD_REQUEST,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+          message: 'Validation failed',
+          errors: validationErrors,
+        });
+        return;
+      }
+    }
+
+    // Si no es un error de validación, manejar como error interno del servidor
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
