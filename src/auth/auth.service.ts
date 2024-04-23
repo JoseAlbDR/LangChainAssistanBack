3fbd13907afaf6bb202c0be274ba9cdd7b83176c
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-23 09:03:02 +0200

    feat: User creation without password hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/shared/services/prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const { username, email } = createUserDto;
 
     const [emailExist, usernameExist] = await this.prismaService.$transaction([
@@ -20,5 +25,16 @@ export class AuthService {
       throw new BadRequestException(
         'El nombre de usuario está siendo utilizado',
       );
+
+    try {
+      const user = await this.prismaService.user.create({
+        data: { ...createUserDto },
+      });
+
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Revisar logs de servidor');
+    }
   }
 }
